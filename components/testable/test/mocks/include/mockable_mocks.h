016effdb0a14bc89c7bae7c1f2a64c5573f3307b
Alejandro Rosso <alejo9118@gmail.com>
2019-10-13 17:16:18 -0500

    Add component mocks.

---
+++ components/testable/test/mocks/include/mockable_mocks.h
@@ -0,0 +1,8 @@
+#ifndef MOCKABLE_MOCKS_H
+#define MOCKABLE_MOCKS_H
+
+#include "fff.h"
+
+DECLARE_FAKE_VALUE_FUNC(int, mockable_function);
+
+#endif /* MOCKABLE_MOCKS_H */
---
+++ components/testable/test/mocks/mockable_mocks.c
@@ -0,0 +1,3 @@
+#include "mockable_mocks.h"
+
+DEFINE_FAKE_VALUE_FUNC(int, mockable_function);
