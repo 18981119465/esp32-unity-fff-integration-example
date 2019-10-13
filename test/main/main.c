8bd5d35fbdf46a3a0331e410046414ad53b32a05
Alejandro Rosso <alejo9118@gmail.com>
2019-10-13 17:15:02 -0500

    Call real functions before running tests.

--- test/main/main.c
+++ test/main/main.c
@@ -4,8 +4,21 @@
 
 #include "unity.h"
 
+#include "mockable.h"
+#include "testable.h"
+
+#include "esp_log.h"
+
+
+static const char TAG[] = "main";
+
 
 void app_main(void)
 {
+  ESP_LOGI(TAG, "%s() - First, call some real functions...", __FUNCTION__);
+  mockable_function();
+  testable_calls_mockable();
+  
+  ESP_LOGI(TAG, "%s() - Then, go to test menu...", __FUNCTION__);
   unity_run_menu();
 }
