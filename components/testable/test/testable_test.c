bb0ca26bcede575a0b43c92551cb5948703473b8
Alejandro Rosso <alejo9118@gmail.com>
2019-10-13 17:13:07 -0500

    Set mocked function return values for each test.

--- components/testable/test/testable_test.c
+++ components/testable/test/testable_test.c
@@ -3,17 +3,20 @@
 #include <string.h>
 
 #include "unity.h"
-
+#include "fff.h"
+#include "mockable_mocks.h"
 #include "testable.h"
 
-
 static const char TAG[] = "[testable]";
+DEFINE_FFF_GLOBALS;
 
 
 TEST_CASE("testable_calls_mockable() "
           "should return true if mockable_function() returns zero",
           TAG)
 {
+  RESET_FAKE(mockable_function);
+  mockable_function_fake.return_val = 0;
   TEST_ASSERT_TRUE(testable_calls_mockable());
 }
 
@@ -21,5 +24,7 @@ TEST_CASE("testable_calls_mockable() "
           "should return false if mockable_function() returns non-zero",
           TAG)
 {
+  RESET_FAKE(mockable_function);
+  mockable_function_fake.return_val = -1;
   TEST_ASSERT_FALSE(testable_calls_mockable());
 }
