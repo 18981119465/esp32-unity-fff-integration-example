2e2e0fbc7d307aee32feb54e3ee8683b87235fa7
Alejandro Rosso <alejo9118@gmail.com>
2019-10-13 17:11:32 -0500

    Define build and clean targets to call custom component makefile.

--- components/testable/test/component.mk
+++ components/testable/test/component.mk
@@ -1,1 +1,17 @@
+COMPONENT_OWNBUILDTARGET := 1
+COMPONENT_OWNCLEANTARGET := 1
 COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
+
+# Source directories with mocks for this test component. 
+export MOCK_SRCDIRS := mocks
+
+# Pass this component source dirs to sub-make
+export COMPONENT_SRCDIRS
+
+.PHONY: build
+build:
+	$(MAKE) -C $(COMPONENT_BUILD_DIR) -f $(PROJECT_PATH)/test_component.mk build
+
+.PHONY: clean
+clean:
+	$(MAKE) -C $(COMPONENT_BUILD_DIR) -f $(PROJECT_PATH)/test_component.mk clean
